82080cd7ad73eda7cac6454eadc8f34108cf5bdd
CELSO ARON PEREIRA SILVA LEME <celso.apsleme@senacsp.edu.br>
2023-05-31 21:17:16 -0300

    darkmode

---
+++ script.js
@@ -0,0 +1,8 @@
+ //va,. let, const
+ const  $html = document.querySelector('html');
+ const $checkbox = document.querySelector('#switch');
+
+ $checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode');
+
+ })
